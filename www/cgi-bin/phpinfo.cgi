d1a6e035d28fd64bcd7a1a430054954a2aee7607
XËŁ <46701041+Axelrond@users.noreply.github.com>
2019-05-24 20:45:46 -0700

    Add files via upload

---
+++ www/cgi-bin/phpinfo.cgi
@@ -0,0 +1,2 @@
+#!"C:/Program Files (x86)/Ampps2/php-5.6/php-cgi.exe" -q 
+<?php phpinfo(); ?>
---
+++ www/index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>Hello, human scum!</title>
+</head>
+<table>
+    <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Age</th>
+</tr>
+<tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+</tr>
+<tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+</tr>
+</table>
+
+<img src="Dog.jpg" alt="Dog Digging a Hole"
+style="width:700px;height:366px" />
+
+<ul>
+    <li>Coffee</li>
+    <li>Tea</li>
+    <li>Milk</li>
+</ul>
+
+<ol>
+    <li>Coffee</li>
+    <li>Tea</li>
+    <li>Milk</li>
+</ol>
+
+<a href="https://www.google.com">This is a link, it takes you to Google.com</a>
+<body>
+    <p>You are on the first page! Congratulations, you aren't a complete screeb...yet.</p>
+   </body>
+</html>
